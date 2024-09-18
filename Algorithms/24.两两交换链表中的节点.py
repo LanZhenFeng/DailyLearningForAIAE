6a43429a5b852812698dffd210e981fdf0f7a35f
Lan Zhenfeng <ls1813747895@gmail.com>
2024-09-18 21:41:25 +0800

    添加了第24题的解法

---
+++ Algorithms/24.两两交换链表中的节点.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode.cn id=24 lang=python3
+#
+# [24] 两两交换链表中的节点
+#
+
+# @lc code=start
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+ 
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # 方法一：迭代
+        '''
+        55/55 cases passed (33 ms)
+        Your runtime beats 85.65 % of python3 submissions
+        Your memory usage beats 5.04 % of python3 submissions (16.5 MB)
+        '''
+        if not head:
+            return head
+        dummy_head = ListNode()
+        dummy_head.next = head
+        p = dummy_head
+        while p.next and p.next.next:
+            temp_p = p.next
+            p.next = p.next.next
+            temp_p.next = p.next.next
+            p.next.next = temp_p
+            p = p.next.next
+        return dummy_head.next
+# @lc code=end
+
