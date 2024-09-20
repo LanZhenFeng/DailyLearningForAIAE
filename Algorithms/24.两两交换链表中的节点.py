eef4640f3b92e43b38695696ee50005c09cd141f
Lan Zhenfeng <ls1813747895@gmail.com>
2024-09-20 22:04:48 +0800

    更新了第24题的解法

--- Algorithms/24.两两交换链表中的节点.py
+++ Algorithms/24.两两交换链表中的节点.py
@@ -20,17 +20,35 @@ class Solution:
         Your runtime beats 85.65 % of python3 submissions
         Your memory usage beats 5.04 % of python3 submissions (16.5 MB)
         '''
-        if not head:
+        # if not head:
+        #     return head
+        # dummy_head = ListNode()
+        # dummy_head.next = head
+        # p = dummy_head
+        # while p.next and p.next.next:
+        #     temp_p = p.next
+        #     p.next = p.next.next
+        #     temp_p.next = p.next.next
+        #     p.next.next = temp_p
+        #     p = p.next.next
+        # return dummy_head.next
+    
+        # 方法二：递归
+        '''
+        55/55 cases passed (35 ms)
+        Your runtime beats 75.7 % of python3 submissions
+        Your memory usage beats 21.4 % of python3 submissions (16.5 MB)
+        '''
+        if not head or not head.next:
             return head
-        dummy_head = ListNode()
-        dummy_head.next = head
-        p = dummy_head
-        while p.next and p.next.next:
-            temp_p = p.next
-            p.next = p.next.next
-            temp_p.next = p.next.next
-            p.next.next = temp_p
-            p = p.next.next
-        return dummy_head.next
+
+        pre = head
+        cur = head.next
+        next = cur.next
+        
+        cur.next = pre
+        pre.next = self.swapPairs(next)
+
+        return cur
 # @lc code=end
 
