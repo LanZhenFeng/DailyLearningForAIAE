133ce0f4779d3cde0e66d146c002a828a42b3122
Lan Zhenfeng <ls1813747895@gmail.com>
2024-09-22 12:16:27 +0800

    添加了第1题的解法

--- Algorithms/1.两数之和.py
+++ Algorithms/1.两数之和.py
@@ -7,5 +7,17 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        '''
+        63/63 cases passed (37 ms)
+        Your runtime beats 84.46 % of python3 submissions
+        Your memory usage beats 8.22 % of python3 submissions (17.7 MB)
+        '''
+        dict1 = {}
+        for i in range(len(nums)):
+            dict1[nums[i]] = i
+        for i in range(len(nums)):
+            if (target - nums[i]) in dict1.keys():
+                if i != dict1[target - nums[i]]:
+                    return list((i, dict1[target - nums[i]]))
 # @lc code=end
 
