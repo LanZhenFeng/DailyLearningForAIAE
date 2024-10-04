e467b57366b164f3e1f4c8639b39def16d129075
Lan Zhenfeng <ls1813747895@gmail.com>
2024-10-04 23:16:40 +0800

    更新了第232题的解法

--- Algorithms/232.用栈实现队列.py
+++ Algorithms/232.用栈实现队列.py
@@ -7,9 +7,53 @@
 # @lc code=start
 class MyQueue:
     # list 
+    '''
+    22/22 cases passed (25 ms)
+    Your runtime beats 98.97 % of python3 submissions
+    Your memory usage beats 22.3 % of python3 submissions (16.6 MB)
+    '''
+    # def __init__(self):
+    #     self.stack1 = [] # 主栈
+    #     self.stack2 = [] # 辅助栈
+    #     self.top = -1   # 主栈指针
+
+    # def push(self, x: int) -> None:
+    #     self.stack1.append(x)
+    #     self.top += 1
+
+    # def pop(self) -> int:
+    #     if self.top != -1:
+    #         for i in range(self.top):
+    #             self.stack2.append(self.stack1.pop())
+    #         return_val = self.stack1.pop()
+    #         for i in range(self.top):
+    #             self.stack1.append(self.stack2.pop())
+    #         self.top -= 1
+    #         return return_val
+
+    # def peek(self) -> int:
+    #     if self.top != -1:
+    #         for i in range(self.top):
+    #             self.stack2.append(self.stack1.pop())
+    #         return_val = self.stack1.pop()
+    #         self.stack1.append(return_val)
+    #         for i in range(self.top):
+    #             self.stack1.append(self.stack2.pop())
+    #         return return_val
+
+    # def empty(self) -> bool:
+    #     return self.top == -1
+    
+    # deque
+    '''
+    22/22 cases passed (20 ms)
+    Your runtime beats 100 % of python3 submissions
+    Your memory usage beats 5.03 % of python3 submissions (16.7 MB)
+    '''
     def __init__(self):
-        self.stack1 = [] # 主栈
-        self.stack2 = [] # 辅助栈
+        from collections import deque
+        self.stack1 = deque() # 主栈
+        self.stack2 = deque() # 辅助栈
         self.top = -1   # 主栈指针
 
     def push(self, x: int) -> None:
