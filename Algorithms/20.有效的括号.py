46b1b1dedf2815cc261eb5f26dcca2a1f19bea38
Lan Zhenfeng <ls1813747895@gmail.com>
2024-10-05 20:31:55 +0800

    更新了第20题的解法

--- Algorithms/20.有效的括号.py
+++ Algorithms/20.有效的括号.py
@@ -7,41 +7,86 @@
 # @lc code=start
 class Solution:
     def isValid(self, s: str) -> bool:
-        # 栈
+        # 栈 deque
         '''
         99/99 cases passed (29 ms)
         Your runtime beats 96.63 % of python3 submissions
         Your memory usage beats 5.48 % of python3 submissions (16.6 MB)
         '''
-        if len(s) % 2 != 0:
-            return False
-        from collections import deque
-        bracketStack = deque()
-        for i in s:
-            if i == "(" or i == "{" or i == "[":
-                bracketStack.append(i)
-            if i == ")":
-                if len(bracketStack) == 0:
-                    return False
-                if len(bracketStack) != 0 and bracketStack.pop() != "(":
-                    return False
+        # if len(s) % 2 != 0:
+        #     return False
+        # from collections import deque
+        # bracketStack = deque()
+        # for i in s:
+        #     if i == "(" or i == "{" or i == "[":
+        #         bracketStack.append(i)
+        #     if i == ")":
+        #         if len(bracketStack) == 0:
+        #             return False
+        #         if len(bracketStack) != 0 and bracketStack.pop() != "(":
+        #             return False
                 
-            if i == "}":
-                if len(bracketStack) == 0:
-                    return False
-                if len(bracketStack) != 0 and bracketStack.pop() != "{":
-                    return False
+        #     if i == "}":
+        #         if len(bracketStack) == 0:
+        #             return False
+        #         if len(bracketStack) != 0 and bracketStack.pop() != "{":
+        #             return False
                 
                 
-            if i == "]":
-                if len(bracketStack) == 0:
-                    return False
-                if len(bracketStack) != 0 and bracketStack.pop() != "[":
-                    return False
+        #     if i == "]":
+        #         if len(bracketStack) == 0:
+        #             return False
+        #         if len(bracketStack) != 0 and bracketStack.pop() != "[":
+        #             return False
                 
-        if len(bracketStack) == 0:
-            return True
-        return False
+        # if len(bracketStack) == 0:
+        #     return True
+        # return False
+
+
+        # 栈 list
+        '''
+        99/99 cases passed (31 ms)
+        Your runtime beats 92.71 % of python3 submissions
+        Your memory usage beats 70.03 % of python3 submissions (16.4 MB)
+        '''
+        # stack = []
+        
+        # for item in s:
+        #     if item == '(':
+        #         stack.append(')')
+        #     elif item == '[':
+        #         stack.append(']')
+        #     elif item == '{':
+        #         stack.append('}')
+        #     elif not stack or stack[-1] != item:
+        #         return False
+        #     else:
+        #         stack.pop()
+        
+        # return True if not stack else False
+
+
+        # 栈+字典
+        '''
+        99/99 cases passed (32 ms)
+        Your runtime beats 90.48 % of python3 submissions
+        Your memory usage beats 34.26 % of python3 submissions (16.4 MB)
+        '''
+        stack = []
+        mapping = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+        }
+        for item in s:
+            if item in mapping.keys():
+                stack.append(mapping[item])
+            elif not stack or stack[-1] != item: 
+                return False
+            else: 
+                stack.pop()
+        return True if not stack else False
 
 # @lc code=end
 
