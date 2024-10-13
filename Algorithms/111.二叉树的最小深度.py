c7cefc9063a994e70d9d6a392c55e05c259073ad
Lan Zhenfeng <ls1813747895@gmail.com>
2024-10-13 21:59:12 +0800

    更新了第111题的解法

--- Algorithms/111.二叉树的最小深度.py
+++ Algorithms/111.二叉树的最小深度.py
@@ -45,21 +45,67 @@ class Solution:
         Your runtime beats 99.73 % of python3 submissions
         Your memory usage beats 7.81 % of python3 submissions (48.6 MB)
         '''
+        # if not root: return 0
+        # min_depth = 0
+        # from collections import deque
+        # queue = deque()
+        # queue.append(root)
+        # while queue:
+        #     size = len(queue)
+        #     min_depth += 1
+        #     for i in range(size):
+        #         node = queue.popleft()
+        #         if node.left: queue.append(node.left)
+        #         if node.right: queue.append(node.right)
+        #         if not node.left and not node.right:
+        #             return min_depth
+        # return min_depth
+
+        # 递归+后序遍历(左右中)  高度->深度
+        '''
+        53/53 cases passed (255 ms)
+        Your runtime beats 57.76 % of python3 submissions
+        Your memory usage beats 57.78 % of python3 submissions (48.4 MB)
+        '''
+
+        # def getDepth(node):
+        #     if not node:
+        #         return 0
+        #     leftDepth = getDepth(node.left)
+        #     rightDepth = getDepth(node.right)
+        #     if not leftDepth and rightDepth:
+        #         return 1 + rightDepth
+        #     elif not rightDepth and leftDepth:
+        #         return 1 + leftDepth
+        #     else:
+        #         depth = 1 + min(leftDepth, rightDepth)
+        #         return depth
+
+        # return getDepth(root)
+
+        # 递归+前序遍历(中左右) 回溯
+        '''
+        53/53 cases passed (230 ms)
+        Your runtime beats 79.54 % of python3 submissions
+        Your memory usage beats 28.06 % of python3 submissions (48.5 MB)
+        '''
         if not root: return 0
-        min_depth = 0
-        from collections import deque
-        queue = deque()
-        queue.append(root)
-        while queue:
-            size = len(queue)
-            min_depth += 1
-            for i in range(size):
-                node = queue.popleft()
-                if node.left: queue.append(node.left)
-                if node.right: queue.append(node.right)
-                if not node.left and not node.right:
-                    return min_depth
-        return min_depth
+        ans = 10000
+
+        def getDepth(node, depth):
+            nonlocal ans
+
+            if not node.left and not node.right:
+                ans = ans if ans < depth else depth
+
+            if node.left:
+                getDepth(node.left, depth + 1)
+            if node.right:
+                getDepth(node.right, depth + 1)
+
+        getDepth(root, 1)
+
+        return ans
 
 
 # @lc code=end
