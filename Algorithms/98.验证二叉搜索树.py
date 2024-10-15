37c28aebd9c36a548ea3e462dad408d52244250c
Lan Zhenfeng <ls1813747895@gmail.com>
2024-10-15 22:18:43 +0800

    更新了第98题的解法

--- Algorithms/98.验证二叉搜索树.py
+++ Algorithms/98.验证二叉搜索树.py
@@ -39,21 +39,40 @@ class Solution:
         Your runtime beats 89.85 % of python3 submissions
         Your memory usage beats 69.02 % of python3 submissions (18.4 MB)
         '''
-        arr = []
+        # arr = []
 
-        def traversal(node, arr):
-            if not node: return None
-            traversal(node.left, arr)
-            arr.append(node.val)
-            traversal(node.right, arr)
+        # def traversal(node, arr):
+        #     if not node: return None
+        #     traversal(node.left, arr)
+        #     arr.append(node.val)
+        #     traversal(node.right, arr)
 
-        traversal(root, arr)
-        for i in range(1, len(arr)):
-            if arr[i] <= arr[i - 1]:
-                return False
+        # traversal(root, arr)
+        # for i in range(1, len(arr)):
+        #     if arr[i] <= arr[i - 1]:
+        #         return False
+        # return True
+
+        # 迭代
+        '''
+        85/85 cases passed (35 ms)
+        Your runtime beats 95.92 % of python3 submissions
+        Your memory usage beats 5.18 % of python3 submissions (18.7 MB)
+        '''
+        stack = []
+        cur_node = root
+        pre_node = None
+        while cur_node or len(stack):
+            if cur_node:
+                stack.append(cur_node)
+                cur_node = cur_node.left
+            else:
+                cur_node = stack.pop()
+                if pre_node and cur_node.val <= pre_node.val:
+                    return False
+                pre_node = cur_node
+                cur_node = cur_node.right
         return True
-
-        # TODO 迭代
 
 
 # @lc code=end
