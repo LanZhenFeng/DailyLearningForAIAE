c4fe79138d85f0accf99d27a6ba8720d85ed46da
Lan Zhenfeng <ls1813747895@gmail.com>
2024-11-13 17:03:16 +0800

    更新了第53题的解法

--- Algorithms/53.最大子数组和.py
+++ Algorithms/53.最大子数组和.py
@@ -31,17 +31,29 @@ class Solution:
         Your runtime beats 99.88 % of python3 submissions
         Your memory usage beats 16.18 % of python3 submissions (30.9 MB)
         '''
-        import sys
-        ans = -sys.maxsize
-        count = 0
-        for i in range(len(nums)):
-            count += nums[i]
-            if count > ans:
-                ans = count
-            if count <= 0: count = 0
-        return ans
+        # import sys
+        # ans = -sys.maxsize
+        # count = 0
+        # for i in range(len(nums)):
+        #     count += nums[i]
+        #     if count > ans:
+        #         ans = count
+        #     if count <= 0: count = 0
+        # return ans
 
         # TODO 动态规划
+        # 时间复杂度：O(n)
+        # 空间复杂度：O(n)
+        '''
+        210/210 cases passed (79 ms)
+        Your runtime beats 63.27 % of python3 submissions
+        Your memory usage beats 98.72 % of python3 submissions (30.2 MB
+        '''
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i - 1] + nums[i], nums[i])
+        return max(dp)
 
 
 # @lc code=end
